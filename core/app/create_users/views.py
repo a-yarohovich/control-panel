b56f22f11bd2249aff277401ea51bdc924799358
AndreyYarohovich <yarohovich@gmail.com>
2018-05-04 19:49:35 +0300

    minor fixes

--- core/app/create_users/views.py
+++ core/app/create_users/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash
+from flask import render_template
 from . import bl_create_users
 from .forms import CreateUsersForm
 from logger import logger
@@ -10,8 +10,6 @@ LOG = logger.LOG
 def create_users_view():
     form = CreateUsersForm()
     if form.validate_on_submit():
-        flash("Valid email")
         return render_template("create_users/create_users_view.html", form=form)
     else:
-        flash("Invalid email")
         return render_template("create_users/create_users_view.html", form=form)
